57e62490358ac19ab869a36d7732b80a87ce9c7c
lujing <lujing@qiyi.com>
2017-12-07 11:15:39 +0800

    1

---
+++ hello.js
@@ -0,0 +1,9 @@
+require('./nino.js')
+require('./style.css')
+
+function hello (str)
+{
+    alert(str);
+}
+
+hello("nino111 4353425");
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	entry:['./src/script/main.js','./src/script/a.js'],
+	output:{
+		path:__dirname+'/dist/js',
+		filename:'nino.js'
+	}}
