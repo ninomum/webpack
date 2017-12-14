809757002d33ece43a022ad28dafd4637c72d4d9
lujing <lujing@qiyi.con>
2017-12-14 10:59:32 +0800

    删除

--- hello.js
+++
@@ -1,9 +0,0 @@
-require('./nino.js')
-require('./style.css')
-
-function hello (str)
-{
-    alert(str);
-}
-
-hello("nino111 4353425");
--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	entry:['./src/script/main.js','./src/script/a.js'],
-	output:{
-		path:__dirname+'/dist/js',
-		filename:'nino.js'
-	}}
